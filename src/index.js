147c40a5316cedfad0d18aeefc649622d10c85e1
Craig Martin <craig@kujoyardwear.com>
2019-01-06 21:52:24 -0500

    add main file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const createBaseBO = require('./bo/base-bo');
+const createBaseDAO = require('./bo/base-dao');
+
+module.exports.createBaseBO = createBaseBO;
+module.exports.createBaseDAO = createBaseDAO;
