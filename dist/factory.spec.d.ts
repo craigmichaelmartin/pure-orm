1529282844204af35f00f32b0f12bd16464a9aac
Craig Martin <craigmichaelmartin@gmail.com>
2022-04-25 11:22:31 -0400

    wip

--- dist/factory.spec.d.ts
+++
@@ -1,1 +0,0 @@
-declare const orm: any;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { create } from './factory';
-export { getPrimaryKey, getProperties, getSqlColumns, getReferences, getDisplayName, getPrefixedColumnNames, getColumns, nestClump, clumpIntoGroups, mapToBos, objectifyDatabaseResult, createFromDatabase, createOneFromDatabase, createOneOrNoneFromDatabase, createManyFromDatabase, getSqlInsertParts, getSqlUpdateParts, getMatchingParts, getMatchingPartsObject, getNewWith, getValueBySqlColumn, getId } from './business-object';
